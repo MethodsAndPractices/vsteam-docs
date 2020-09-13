f2a471db06ba1d3b0a35a2cd4d3903f1f5a42331
Sebastian Schütze <sebastian.schuetze@razorspoint.com>
2020-09-13 16:03:39 +0200

    added YAML header generation for modules

--- tools/ci/Build-ModuleHelp.ps1
+++ tools/ci/Build-ModuleHelp.ps1
@@ -16,6 +16,9 @@ if (-not (Get-Module platyPS -ListAvailable)) {
 $OutputFolder = "./modules/$($Module.ToLower())"
 
 New-MarkdownHelp -Module $Module -OutputFolder $OutputFolder -Force
+
+. $PSScriptRoot\Add-CustomYamMeta.ps1 -Path $OutputFolder -BaseEditUrlPath "https://github.com/MethodsAndPractices/$($Module.ToLower())/edit/master/.docs"
+
 . $PSScriptRoot\Create-DocusaurusMenuFile.ps1 -MarkdownPath $OutputFolder
 
 . $PSScriptRoot\Create-ModuleIndex.ps1 -Module $Module
