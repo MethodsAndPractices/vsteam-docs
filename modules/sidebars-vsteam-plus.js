5684f09f26959d4a0624ac010f86d9ddf31fd58d
Sebastian S Schütze <sebastian.s.schuetze@deutschebahn.com>
2020-09-16 20:35:23 +0200

    vsteam plus sidebar

---
+++ modules/sidebars-vsteam-plus.js
@@ -0,0 +1,11 @@
+//let vsTeamModuleCommands = require("./vsteam-plus/commands/sidebars.js");
+
+//let cmdlets = [...vsTeamModuleCommands]
+
+module.exports = {
+    modules: {
+        "Getting Started": [
+            'introduction'
+        ]
+    }
+};
