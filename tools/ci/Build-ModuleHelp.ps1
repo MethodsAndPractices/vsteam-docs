a7cb8b227887f8841ede3aa298b0da3ab00dd39a
Donovan Brown <dbrown@microsoft.com>
2020-10-02 19:10:52 -0500

    Fixed Edit Path

--- tools/ci/Build-ModuleHelp.ps1
+++ tools/ci/Build-ModuleHelp.ps1
@@ -1,23 +1,24 @@
 [CmdletBinding()]
 param (
-    [Parameter(Mandatory=$false)]
-    [string]
-    $Module
+   [Parameter(Mandatory = $false)]
+   [string]
+   $Module
 )
 
 if (-not (Get-Module $Module -ListAvailable)) {
-    Install-Module $Module -Scope CurrentUser -Force
+   Install-Module $Module -Scope CurrentUser -Force
 }
 
 if (-not (Get-Module platyPS -ListAvailable)) {
-    Install-Module platyPS -Scope CurrentUser -Force
+   Install-Module platyPS -Scope CurrentUser -Force
 }
 
 $OutputFolder = "./docs/modules/$($Module.ToLower())/commands"
 
 New-MarkdownHelp -Module $Module -OutputFolder $OutputFolder -Force
 
-. $PSScriptRoot\Add-CustomYamMeta.ps1 -Path $OutputFolder -BaseEditUrlPath "https://github.com/MethodsAndPractices/$($Module.ToLower())/edit/master/.docs"
+. $PSScriptRoot\Add-CustomYamMeta.ps1 -Path $OutputFolder `
+   -BaseEditUrlPath "https://github.com/MethodsAndPractices/$($Module.ToLower())/edit/trunk/.docs"
 
 . $PSScriptRoot\Create-DocusaurusMenuFile.ps1 -MarkdownPath $OutputFolder -NavigationPath "modules/vsteam/commands"
 
